3613fae843d7983316af60abe54e1bf40f66b9ae
Ott Tooming <OttTooming@gmail.com>
2015-02-01 13:11:30 +0200

    Add social widget
    Usability needs to be improved. Req addition: profile image field

--- functions.php
+++ functions.php
@@ -361,4 +361,143 @@ function wp_bootstrap_pagination( $args = array() ) {
     }
     if ( isset($echo) )
         echo $args['before_output'] . $echo . $args['after_output'];
+};
+
+
+/****************************************************************/
+
+
+/*
+  Widget Name: KHK tudengifirmad
+  Description: Tudengifirmasid tutvustav ja kontakte sisaldav widget
+ */
+
+/**
+ * Adds khk_tudengifirmad widget.
+ */
+class khk_tudengifirmad extends WP_Widget {
+
+    /**
+     * Register widget with WordPress.
+     */
+    function __construct() {
+        parent::__construct(
+                'khk_tudengifirmad',
+                __('KHK tudengifirmad', 'translation_domain'), // Name
+                array('description' => __('Links to Author social media profile', 'translation_domain'),)
+        );
+    }
+
+    /**
+     * Front-end display of widget.
+     *
+     * @see WP_Widget::widget()
+     *
+     * @param array $args     Widget arguments.
+     * @param array $instance Saved values from database.
+     */
+    public function widget($args, $instance) {
+
+        $title = apply_filters('widget_title', $instance['title']);
+        $facebook = $instance['facebook'];
+        $twitter = $instance['twitter'];
+        $google = $instance['google'];
+        $linkedin = $instance['linkedin'];
+
+        // social profile link
+        $facebook_profile = '<a class="facebook" href="' . $facebook . '"><i class="fa fa-facebook"></i></a>';
+        $twitter_profile = '<a class="twitter" href="' . $twitter . '"><i class="fa fa-twitter"></i></a>';
+        $google_profile = '<a class="google" href="' . $google . '"><i class="fa fa-google-plus"></i></a>';
+        $linkedin_profile = '<a class="linkedin" href="' . $linkedin . '"><i class="fa fa-linkedin"></i></a>';
+
+        echo $args['before_widget'];
+
+        if (!empty($title)) {
+            echo $args['before_title'] . $title . $args['after_title'];
+        }
+
+        echo '<div class="social-icons">';
+        echo (!empty($facebook) ) ? $facebook_profile : null;
+        echo (!empty($twitter) ) ? $twitter_profile : null;
+        echo (!empty($google) ) ? $google_profile : null;
+        echo (!empty($linkedin) ) ? $linkedin_profile : null;
+        echo '</div>';
+
+        echo $args['after_widget'];
+    }
+
+    /**
+     * Back-end widget form.
+     *
+     * @see WP_Widget::form()
+     *
+     * @param array $instance Previously saved values from database.
+     */
+    public function form($instance) {
+        isset($instance['title']) ? $title = $instance['title'] : null;
+        empty($instance['title']) ? $title = 'My Social Profile' : null;
+
+        isset($instance['facebook']) ? $facebook = $instance['facebook'] : null;
+        isset($instance['twitter']) ? $twitter = $instance['twitter'] : null;
+        isset($instance['google']) ? $google = $instance['google'] : null;
+        isset($instance['linkedin']) ? $linkedin = $instance['linkedin'] : null;
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>">
+        </p>
+
+        <p>
+            <label for="<?php echo $this->get_field_id('facebook'); ?>"><?php _e('Facebook:'); ?></label> 
+            <input class="widefat" id="<?php echo $this->get_field_id('facebook'); ?>" name="<?php echo $this->get_field_name('facebook'); ?>" type="text" value="<?php echo esc_attr($facebook); ?>">
+        </p>
+
+        <p>
+            <label for="<?php echo $this->get_field_id('twitter'); ?>"><?php _e('Twitter:'); ?></label> 
+            <input class="widefat" id="<?php echo $this->get_field_id('twitter'); ?>" name="<?php echo $this->get_field_name('twitter'); ?>" type="text" value="<?php echo esc_attr($twitter); ?>">
+        </p>
+
+        <p>
+            <label for="<?php echo $this->get_field_id('google'); ?>"><?php _e('Google+:'); ?></label> 
+            <input class="widefat" id="<?php echo $this->get_field_id('google'); ?>" name="<?php echo $this->get_field_name('google'); ?>" type="text" value="<?php echo esc_attr($google); ?>">
+        </p>
+
+        <p>
+            <label for="<?php echo $this->get_field_id('linkedin'); ?>"><?php _e('Linkedin:'); ?></label> 
+            <input class="widefat" id="<?php echo $this->get_field_id('linkedin'); ?>" name="<?php echo $this->get_field_name('linkedin'); ?>" type="text" value="<?php echo esc_attr($linkedin); ?>">
+        </p>
+
+        <?php
+    }
+
+    /**
+     * Sanitize widget form values as they are saved.
+     *
+     * @see WP_Widget::update()
+     *
+     * @param array $new_instance Values just sent to be saved.
+     * @param array $old_instance Previously saved values from database.
+     *
+     * @return array Updated safe values to be saved.
+     */
+    public function update($new_instance, $old_instance) {
+        $instance = array();
+        $instance['title'] = (!empty($new_instance['title']) ) ? strip_tags($new_instance['title']) : '';
+        $instance['facebook'] = (!empty($new_instance['facebook']) ) ? strip_tags($new_instance['facebook']) : '';
+        $instance['twitter'] = (!empty($new_instance['twitter']) ) ? strip_tags($new_instance['twitter']) : '';
+        $instance['google'] = (!empty($new_instance['google']) ) ? strip_tags($new_instance['google']) : '';
+        $instance['linkedin'] = (!empty($new_instance['linkedin']) ) ? strip_tags($new_instance['linkedin']) : '';
+
+        return $instance;
+    }
+
 }
+
+// register khk_tudengifirmad widget
+function register_khk_tudengifirmad() {
+    register_widget('khk_tudengifirmad');
+}
+
+add_action('widgets_init', 'register_khk_tudengifirmad');
+
+
