e816f5a12116c2c663029205199a7e808d1c1f9c
Ott Tooming <OttTooming@gmail.com>
2015-02-27 15:55:08 +0200

    Add slide elements into view
    CSS transform to slide elements into view.
    JS to check whether elements are within viewport.

--- js/theme.js
+++ js/theme.js
@@ -48,3 +48,59 @@ $(document).ready(function() {
   $(window).resize(myResizeFunction).trigger('resize');
 });
 
+
+
+
+
+/**
+*
+* @desc A small plugin that checks whether elements are within
+*     the user visible viewport of a web browser.
+*     only accounts for vertical position, not horizontal.
+*
+* @author Sam Sehnert
+* Copyright 2012, Digital Fusion
+* Licensed under the MIT license.
+* http://teamdf.com/jquery-plugins/license/
+*/
+
+(function($) {
+
+  $.fn.visible = function(partial) {
+    
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+    
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+    
+})(jQuery);
+
+var win = $(window);
+
+var allMods = $(".panel-grid-cell");
+
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible"); 
+  } 
+});
+
+win.scroll(function(event) {
+  
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+  
+});
